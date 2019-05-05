eff33373db927142edef344dbb0866d5359f3c34
mariusonu <onu.marius777@gmail.com>
2019-05-05 19:03:55 +0300

    ex 12 switch

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,43 @@ public class Main {
     public static void main(String[] args) {
         int n = 7;
         String anotimp;
-        if (n == 0) {
-            anotimp ="iarna";
-        } else if (n == 1) {
-            anotimp = "iarna";
-        } else if (n == 2) {
-            anotimp = "primavara";
-        } else if (n == 3){
-            anotimp = "primavara";
-        }else if (n == 4){
-            anotimp ="primavara";
-        } else if (n == 5){
-            anotimp = "vara";
-        } else if (n == 6){
-            anotimp = "vara";
-        }else if (n == 7){
-            anotimp = "vara";
-        }else if (n == 8){
-            anotimp = "toamna";
-        }else if (n == 9){
-            anotimp = "toamna";
-        }else if (n == 10){
-            anotimp = "toamna";
-        }else if (n == 11){
-            anotimp = "iarna";
-        } else {
-            anotimp = "ai iesit din calendar";
+        switch (n) {
+            case 1:
+                anotimp = "iarna";
+                break;
+            case 2:
+                anotimp = "iarna";
+                break;
+            case 3:
+                anotimp = "primavara";
+                break;
+            case 4:
+                anotimp = "primavara";
+                break;
+            case 5:
+                anotimp = "primavara";
+                break;
+            case 6:
+                anotimp = "vara";
+                break;
+            case 7:
+                anotimp = "vara";
+                break;
+            case 8:
+                anotimp = "vara";
+                break;
+            case 9:
+                anotimp = "toamna";
+                break;
+            case 10:
+                anotimp = "toamna";
+            case 11:
+                anotimp = "toamna";
+            case 12:
+                anotimp = "iarna";
+            default:
+                anotimp = "numar gresit";
+                break;
         }
         System.out.println(anotimp);
 
