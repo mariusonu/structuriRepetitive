ce15cd8085451f66e66a82e08274a10ce05d9fc7
mariusonu <onu.marius777@gmail.com>
2019-05-05 19:44:39 +0300

    ex 14

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,14 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int[] numerePare = {2, 4, 6, 8, 11, 13, 15};
-        System.out.println("Numerele pare sunt: ");
+        int n = 1;
+        while ( n < 8) {
+            System.out.println("going");
+            n++;
 
-        for (int i = 0; i < numerePare.length; i++) {
-            if (numerePare[i] % 2 == 0) {
-                System.out.println(numerePare[i]);
-            }
         }
-        System.out.println("Numerele impare sunt: ");
-
-        for (int i = 0; i < numerePare.length; i++) {
-            if (numerePare[i] %2 != 0) {
-                System.out.println(numerePare[i]);
-            }
-        }
-    }
 
 
     }
 
+}
