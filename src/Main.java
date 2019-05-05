ff3c4184497a0440387a03bb1862f28d4239c846
mariusonu <onu.marius777@gmail.com>
2019-05-05 20:38:42 +0300

    ex 9

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int n = 1;
-        while ( n < 8) {
-            System.out.println("going");
-            n++;
+        int rows = 7;
 
+        for(int i = 1; i <= rows; ++i) {
+            for(int j = 1; j <= i; ++j) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
         }
-
-
     }
-
 }
