8552a508da3ca3be1ad74d5ffa6e92e7b5524c4c
mariusonu <onu.marius777@gmail.com>
2019-05-05 18:59:56 +0300

    ex 12 if else

--- src/Main.java
+++ src/Main.java
@@ -3,40 +3,35 @@ public class Main {
 
     public static void main(String[] args) {
         int n = 7;
-        String numar;
-        switch (n) {
-            case 1:
-                numar = "unu";
-                break;
-            case 2:
-                numar = "doi";
-                break;
-            case 3:
-                numar = "trei";
-                break;
-            case 4:
-                numar = "patru";
-                break;
-            case 5:
-                numar = "cinci";
-                break;
-            case 6:
-                numar = "sase";
-                break;
-            case 7:
-                numar = "sapte";
-                break;
-            case 8:
-                numar = "opt";
-                break;
-            case 9:
-                numar = "noua";
-                break;
-            default:
-                numar = "numar gresit";
-                break;
+        String anotimp;
+        if (n == 0) {
+            anotimp ="iarna";
+        } else if (n == 1) {
+            anotimp = "iarna";
+        } else if (n == 2) {
+            anotimp = "primavara";
+        } else if (n == 3){
+            anotimp = "primavara";
+        }else if (n == 4){
+            anotimp ="primavara";
+        } else if (n == 5){
+            anotimp = "vara";
+        } else if (n == 6){
+            anotimp = "vara";
+        }else if (n == 7){
+            anotimp = "vara";
+        }else if (n == 8){
+            anotimp = "toamna";
+        }else if (n == 9){
+            anotimp = "toamna";
+        }else if (n == 10){
+            anotimp = "toamna";
+        }else if (n == 11){
+            anotimp = "iarna";
+        } else {
+            anotimp = "ai iesit din calendar";
         }
-        System.out.println(numar);
+        System.out.println(anotimp);
 
     }
 }
