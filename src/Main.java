1e9170f44c775747301c64cf0a7a03908ca46b4c
mariusonu <onu.marius777@gmail.com>
2019-05-05 18:58:17 +0300

    ex 11 if else

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,32 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int rows = 6;
-
-        for (int i = 1; i <= rows; ++i) {
-            for (int j = 1; j <= i; ++j) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
+        int n=7;
+        String numar;
+        if  (n == 0) {
+            numar = "zero";
+        } else if (n == 1){
+            numar = "unu";
+        } else if (n == 2){
+            numar = "doi";
+        } else if (n == 3){
+            numar = "trei";
+        } else if (n == 4){
+            numar = "patru";
+        } else if (n == 5){
+            numar ="cinci";
+        } else if (n == 6){
+            numar ="sase";
+        } else if (n == 7){
+            numar = "sapte";
+        } else if (n == 8){
+            numar ="opt";
+        } else if (n == 9){
+            numar ="noua";
+        } else {
+            numar ="numar gresit";
         }
+        System.out.println(numar);
+
     }
 }
